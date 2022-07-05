f9896cb954ce9392b88714aef3ed92100d416142
kubokohei <85267997+kubokohei@users.noreply.github.com>
2022-07-06 00:29:41 +0900

    素のJSはdocument.が基本なのかな、プロパティ適切に使うの大変そうだな

--- src/index.js
+++ src/index.js
@@ -11,7 +11,13 @@ const onClickAdd = () => {
 
   // liタグ生成
   const li = document.createElement("li");
-  li.innerText = "";
+  li.innerText = inputText;
+
+  // divタグの子要素に各要素を設定
+  div.appendChild(li);
+
+  // 未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(div);
 };
 
 document
